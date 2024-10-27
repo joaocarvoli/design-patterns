bcc2507f4b2e0c510b029a009bcb9f15e67e80ba
João Victor <joaocarvoli@hotmail.com>
2024-10-27 11:18:04 -0300

    observer: setting up files to examples

---
+++ behavioral/observer/before/main.py
@@ -0,0 +1,13 @@
+# Before code from https://github.com/ArjanCodes/betterpython/tree/main/4%20-%20observer%20pattern
+
+from api.user import register_new_user, password_forgotten
+from api.plan import upgrade_plan
+
+# register a new user
+register_new_user("Arjan", "BestPasswordEva", "hi@arjanegges.com")
+
+# send a password reset message
+password_forgotten("hi@arjanegges.com")
+
+# upgrade the plan
+upgrade_plan("hi@arjanegges.com")
